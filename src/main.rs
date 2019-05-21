4cd6c4d992ba88f871a7553829a1c50feb5f4366
David Alber <alber.david@gmail.com>
2019-05-21 12:50:51 -0700

    Compute min and max

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,48 @@ extern crate clap;
 use clap::{App, SubCommand};
 use std::io::Read;
 
-fn load_data() -> std::io::Result<Vec<f64>> {
+#[derive(Debug)]
+struct Data {
+    data: Vec<f64>,
+    min_val: f64,
+    max_val: f64,
+}
+
+impl Data {
+    fn new(data: Vec<f64>) -> Self {
+        let mut max_val = std::f64::MIN;
+        let mut min_val = std::f64::MAX;
+
+        for val in data.iter() {
+            if *val > max_val {
+                max_val = *val;
+            }
+            if *val < min_val {
+                min_val = *val;
+            }
+        }
+
+        Data {
+            data,
+            min_val,
+            max_val,
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    fn min(&self) -> f64 {
+        self.min_val
+    }
+
+    fn max(&self) -> f64 {
+        self.max_val
+    }
+}
+
+fn load_data() -> std::io::Result<Data> {
     let mut data: Vec<f64> = Vec::new();
     let mut buffer = String::new();
     std::io::stdin().read_to_string(&mut buffer)?;
@@ -25,7 +66,7 @@ fn load_data() -> std::io::Result<Vec<f64>> {
         }
     }
 
-    Ok(data)
+    Ok(Data::new(data))
 }
 
 fn main() -> Result<(), i32> {
@@ -39,7 +80,7 @@ fn main() -> Result<(), i32> {
         )
         .get_matches();
 
-    let data: Vec<f64> = match load_data() {
+    let data: Data = match load_data() {
         Ok(d) => d,
         Err(e) => {
             println!("{}", e);
@@ -49,6 +90,12 @@ fn main() -> Result<(), i32> {
 
     if matches.subcommand_matches("histogram").is_some() {
         println!("{:?}", data);
+        println!(
+            "# NumSamples = {}; Min = {:.2}; Max = {:.2}",
+            data.len(),
+            data.min(),
+            data.max()
+        );
     }
 
     Ok(())
